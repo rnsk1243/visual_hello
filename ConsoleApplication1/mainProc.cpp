826a064d8e6c042bef6fd56052555d73a6937793
WooHee Lee <rnsk8945@gmail.com>
2016-06-28 00:16:08 +0900

    하 시발

--- ConsoleApplication1/mainProc.cpp
+++ ConsoleApplication1/mainProc.cpp
@@ -1,11 +1,1 @@
-#include"Hello.h"
-
-void main()
-{
-	// 나는 차였다
-	CHello haro;
-	haro.hello();
-	// 정말로 차였다.
-	// 왜 일까? 흑흑
-	// gg
-}
+﻿#include"Hello.h"  void main() { 	// ?섎뒗 李⑥??? 	CHello haro; 	haro.hello(); 	// ?뺣쭚濡?李⑥??? 	// ???쇨퉴? ?묓쓳 	// gg } 
