0b8fbd03b5dc1e1595599b69f64a9ad83b897659
WooHee Lee <rnsk8945@gmail.com>
2016-06-27 22:53:11 +0900

    I was Car 나는 차였다 처음 커밋임

---
+++ ConsoleApplication1/Hello.cpp
@@ -0,0 +1,17 @@
+#include "Hello.h"
+
+
+
+CHello::CHello()
+{
+}
+
+
+CHello::~CHello()
+{
+}
+
+void CHello::hello()
+{
+	cout << "나는 차였다.." << endl;
+}
---
+++ ConsoleApplication1/Hello.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<iostream>
+using namespace std;
+class CHello
+{
+public:
+	CHello();
+	~CHello();
+	void hello();
+};
+
